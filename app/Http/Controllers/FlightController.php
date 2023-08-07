33fdefee683aa517e6865671a771416e75b1c8dd
x10combo <rakelspahi03@gmail.com>
2023-08-07 19:31:56 +0200

    Added parameters for controller and made GET req.

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Process;
 
 class FlightController extends Controller
 {
@@ -12,18 +11,36 @@ class FlightController extends Controller
         $departure = $request->input('departure');
         $arrival = $request->input('arrival');
         $departure_date = $request->input('departure_date');
+        $url = 'https://www.ryanair.com/api/farfnd/3/oneWayFares';
 
-        $process = new Process([
-            'node',
-            'puppeteer_script.js',
-            $departure,
-            $arrival,
-            $departure_date,
-        ]);
+        // GET request parameters
+        $params = [
+            'ToUs' => 'AGREED',
+            'arrivalAirportIataCode' => $arrival,
+            'departureAirportIataCode' => $departure,
+            'language' => 'en',
+            'limit' => 16,
+            'market' => 'en-gb',
+            'offset' => 0,
+            'outboundDepartureDateFrom' => $departure_date,
+            'outboundDepartureDateTo' => $departure_date,
+            'priceValueTo' => 150,
+        ];
 
-        $process->run();
-        $flightData = $process->getOutput();
+        $client = new Client();
 
-        return response()->json(['flights' => json_decode($flightData)], 200);
+        try {
+            // Make the GET request
+            $response = $client->request('GET', $url, ['query' => $params]);
+
+            // Get the response body
+            $flightData = $response->getBody()->getContents();
+
+            // Return the response as JSON
+            return response()->json(['flights' => json_decode($flightData)], 200);
+        } catch (\Exception $e) {
+            // Handle any errors that occurred during the request
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
